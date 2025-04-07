f5a8ff97c0d67f076c95d45ddefafe71230bf1a4
Artur Prepelytsia <preart81@gmail.com>
2025-04-08 00:29:23 +0300

    Handle database connection errors by exiting the process

--- db/Sequelize.js
+++ db/Sequelize.js
@@ -17,6 +17,7 @@ try {
     console.log("Database connection successful");
 } catch (error) {
     console.error("Unable to connect to the database:", error.message);
+    process.exit(1);
 }
 
 export default sequelize;
