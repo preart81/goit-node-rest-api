b8ca5c09c3fe5fdb9738abf55ffc2cc98d6e9e9c
Artur Prepelytsia <preart81@gmail.com>
2025-04-27 04:44:03 +0300

    feat: added functionality to update user avatar
    - Created PATCH /auth/avatars endpoint for updating avatar
    - Added handling of uploading files via multer
    - Implemented saving files to temp and public/avatars
    - Static files distribution is configured
    - Updated logic for saving avatarURL in the database

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,5 +1,9 @@
+import fs from "fs/promises";
+import path from "path";
 import ctrlWrapper from "../helpers/ctrlWrapper.js";
 import * as authServices from "../services/authServices.js";
+
+const avatarsDir = path.join(process.cwd(), "public", "avatars");
 
 const registerUserController = async (req, res) => {
     const NewUser = await authServices.registerUser(req.body);
@@ -33,6 +37,30 @@ const getCurrentUserController = async (req, res) => {
     });
 };
 
+const updateAvatarUserController = async (req, res) => {
+    if (!req.file) {
+        throw HttpError(400, "Аватар не завантажено");
+    }
+    const { path: tempUpload, originalname } = req.file;
+    const { id } = req.user;
+    // Створюємо унікальне ім'я файлу
+    const filename = `${id}_${originalname}`;
+    const resultUpload = path.join(avatarsDir, filename);
+
+    // Переміщуємо файл з temp в public/avatars
+    await fs.rename(tempUpload, resultUpload);
+
+    // Формуємо URL для аватара
+    const avatarURL = path.join("avatars", filename);
+
+    // Оновлюємо URL в базі даних
+    await req.user.update({ avatarURL });
+
+    res.status(200).json({
+        avatarURL,
+    });
+};
+
 const logoutUserController = async (req, res) => {
     const { id } = req.user;
     await authServices.logoutUser(id);
@@ -44,5 +72,6 @@ export default {
     registerUserController: ctrlWrapper(registerUserController),
     loginUserController: ctrlWrapper(loginUserController),
     getCurrentUserController: ctrlWrapper(getCurrentUserController),
+    updateAvatarUserController: ctrlWrapper(updateAvatarUserController),
     logoutUserController: ctrlWrapper(logoutUserController),
 };
--- routes/authRouter.js
+++ routes/authRouter.js
@@ -2,8 +2,8 @@ import express from "express";
 import authControllers from "../controllers/authControllers.js";
 import validateBody from "../helpers/validateBody.js";
 import authenticate from "../middlewares/authenticate.js";
+import upload from "../middlewares/upload.js";
 import { authLoginSchema, authRegisterSchema } from "../schemas/authSchemas.js";
-
 const authRouter = express.Router();
 
 authRouter.post(
@@ -14,6 +14,13 @@ authRouter.post(
 authRouter.post("/login", validateBody(authLoginSchema), authControllers.loginUserController);
 
 authRouter.get("/current", authenticate, authControllers.getCurrentUserController);
+
+authRouter.patch(
+    "/avatars",
+    authenticate,
+    upload.single("avatar"),
+    authControllers.updateAvatarUserController
+);
 
 authRouter.post("/logout", authenticate, authControllers.logoutUserController);
 
